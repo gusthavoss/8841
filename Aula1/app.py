94b7ea33a872cae425e8c212faee4d8db9743b32
Gusthavo Silva <gusthavo.silva@nubank.com.br>
2026-01-26 19:26:35 -0300

    segundo commit

--- Aula1/app.py
+++ Aula1/app.py
@@ -2,3 +2,90 @@
 
 print("Olá Mundo!")
 print("Meu nome é gusthavo")
+
+# INPUT é o comando utilizado pra coletar informações via terminal
+
+input("Qual o seu nome? ")
+
+# CTRL + S + Salvar as alterações
+# CTRL + L = Limpar o terminal
+
+#VARIÁVEIS são palavras que armazenam um valor
+
+nome = "Gusthavo Souza"
+print("É um prazer tê-lo aqui, ",nome)
+print("É um prazer tê-lo aqui, {}".format(nome))
+print(f"É um prazer tê-lo aqui, {nome}")
+
+
+
+
+
+
+
+
+nome = "Husthavo Silva" #String
+
+
+
+
+
+
+
+numero01 =10
+numero02 = 5
+
+numero03 = numero01 + numero02
+print(numero03)
+
+#========================================
+
+nome01 = "Gusthavo"
+nome02 = 10
+
+nome03 = nome01 * nome02
+
+#========================================
+
+palavra = "500"
+numero = int(palavra)
+print(numero)
+
+#========================================
+# Métodos disponiveis pra STR
+nome_completo = "Gusthavo Souza e Silva"
+
+nome_completo01 = come_completo.lower()
+print(nome_completo01)
+
+nome_completo03 = nome_completo.replace("i","h")
+print(nome_completo03)
+
+nome_completo04 = nome_completo.isnumeric()
+print(nome_completo04)
+
+
+
+
+
+# ====================================== 
+# Estruturas de decisão
+
+idaide = 29
+if idade > 18:
+    print("Você é maior de idaide.>")
+    print("Você já pode dirigir!")
+
+elif idade > 15:
+    print("Você não é maior de idade, mas já é um adolecente")
+
+elif idade > 10:
+    print("Você é novo de mais")
+
+else:
+    print("Você é novo de mais")
+
+print("Esta linha sempre vai ser executada, e não depende do resultado do 'if'")
+
+# ====================================== 
+#Estrutura de repetição
--- Aula1/exercicios01.py
+++ Aula1/exercicios01.py
@@ -9,8 +9,49 @@
 # Idade: 29
 # ------------------------------
 
-input("Nome: ")
-input("Idade: ")
-input("CPF: ")
+nome = input("Nome: ")
+idade = input("Idade: ")
+cpf = input("CPF: ")
 
-print(" ------------------------------\nConfirmação de cadastro:\n Nome: Tiago P. Lima\n CPF: 132.465.789-45\n Idade: 29\n ------------------------------")
+print(f" ------------------------------\nConfirmação de cadastro:\n Nome: {nome}\n CPF: {cpf}\n Idade: {idade}\n ------------------------------")
+
+
+
+# 2) Escreva um script em Python que receba dois numeros e que realize as seguintes operações com eles:
+# - Soma dos dois numeros
+# - Diferença dos dois numeros
+# Os resultados deverão ser apresentados no formato:
+# 4 + 2 = 6
+# 4 - 2 = 2
+
+num1 = float(input("Numero 1: "))
+num2 = float(input("Numero 2: "))
+
+num3 = num1 + num2
+num4 = num1 - num2
+print(f"{num1} + {num2} = {num3}\n{num1} + {num2} = {num4}")
+
+# 3) Escreva um script em Python que traduza a seguinte frase em uma frase legível, removendo os "x" dela:
+# Umxpratoxdextrigoxparaxtrêsxtigresxtristes
+
+frase = "Umxpratoxdextrigoxparaxtrêsxtigresxtristes"
+frasetradusida = frase.replace("x"," ")
+
+# 4) Escreva um script em Python que receba o ano de nascimento do usuário, e que retorne à qual geração esse usuário pertence, se baseando na idade. Utilize a tabela a seguir pra calcular:
+
+# Geração    Intervalo
+# Boomer     Antes de 1964
+# Geração X  1965 - 1979
+# Geração Y  1980 - 1994
+# Geraçãp Z  1995 - Atual
+
+ano = int(input("Que ano você nasceu: "))
+if ano<1965:
+    print("Boomer")
+elif ano<1979:
+    print("Geração X")
+elif ano<1994:
+    print("Geração Y")
+else:
+    print("Geração Z")
+
