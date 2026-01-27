da167545c37462eb70436ba049f2ce4726a91aa7
Gusthavo Silva <gusthavo.silva@nubank.com.br>
2026-01-27 20:03:56 -0300

    aula 2 commit final

--- Aula02/app.py
+++ Aula02/app.py
@@ -25,6 +25,7 @@ while True:
         continue
     print(numeral)
     numeral += 1 # faz o mesmo que numeral = numeral + 1
+    break
 
 # FOR (para) ===================
 numero = 0
@@ -35,3 +36,71 @@ nome = "Tiago"
 
 for letra in nome:
     print(letra)
+
+# Coleções =====================
+
+# Tuplas / Listas / Dicionários / Sets
+
+#Tuplas
+# Tuplas são coleções de objetos no python que não podem ser alteradas.
+
+tupla = (45, 111, True, "Ghus", "Kaina", 96.3) # Tuplas são criadas com parentesis
+# INDEX  0    1     2     3        4      5
+
+print(tupla)
+print(tupla[3])
+
+for objeto in tupla:
+    print(f"Valor recebido pela variavel do for vindo da tupla {objeto}")
+
+#LISTAS
+# Listas são muito semelhantes à tuplas, porem são bem mais flexiveis.
+
+lista = [23, 55, false, "Roberto", "Alessandra", 85.663, tupla] # Listas são criadas com colchetes
+# INDEX.  1   2.   3.       4.           5.         6
+
+print(lista)
+print(lista[5])
+print(lista[6][2])
+
+lista.append(True)
+print(lista)
+
+lista.insert(2, "Novo nome")
+print(lista)
+
+lista.pop(1)
+print(lista)
+
+posicao = lista.index("Alessandra")
+print(posicao)
+
+# DICIONÁRIOS -------------
+# Dicionários são coleções estruturadas em pares de {"Chave": "Valor"}
+
+#                 ITEM             ITEM          ITEM               ITEM
+dicionario = {"Nome":"Tiago", "Idade": 29, "Altura": 1.89, "Masculino": True}
+#              CHAVE: VALOR.    CHAVE: VALOR.  CHAVE: VALOR.     CHAVE: VALOR
+
+# Chaves em dicionários são UNICAS.
+# Dicionários não possuem um INDEX.
+
+print(dicionario)
+print(dicionario["Nome"])
+
+print(dicionario.items())
+print(dicionario.keys)
+print(dicionario.values)
+
+dicionario["Cor"] = "Vermelho"
+print(dicionario)
+
+for chave, valor in dicionario:
+    print(f"A chave {chave} tem o valor {valor}")
+
+#-----
+# SETS
+# Sets são coleções de chaves de dicionários
+
+meu_set = {"Tiago","Jovem","Moreno"}
+print(meu_set)
--- Aula02/exercicios.py
+++ Aula02/exercicios.py
@@ -1,4 +1,4 @@
-# 1) Escreva em script em Python que represente uma quitanda. O seu script deverá apresentar
+# 4) Escreva em script em Python que represente uma quitanda. O seu script deverá apresentar
 # opções de frutas, e cada vez que voce escolher a fruta desejada, a fruta deverá ser
 # adicionada a uma cesta de compras.
 
@@ -21,9 +21,41 @@
 # notifique. O programa só deverá finalizar caso a pessoa digite o numero 3 no menu 
 # principal.
 
+#/opcao = 0
+#cesta = ""
+#cesta_pos = 0
+#fruta = 0
+#
+#while opcao < 3:
+#    opcao = int(input("Menu Principal:\n\n1: Ver cesta\n2: Adicionar uma fruta\n3: Sair\n"))
+#
+#    if opcao == 1:
+#        print(cesta)
+    
+
+#    if opcao == 2:
+#        fruta = int(input("Menu de frutas:\nDigite a opção que deseja adicionar:\n1 - Banana\n2 - Melancia\n3 - Morango\n"))
+#        
+#        if fruta == 1:
+#           cesta += "Banana, " 
+#           print("Banana adicionada com sucesso")
+#        elif fruta == 2:
+#           cesta += "Melancia, " 
+#           print("Melancia adicionada com sucesso")
+#        elif fruta == 3:
+#           cesta += "Morango, " 
+#           print("Morango adicionada com sucesso")
+#        else:
+#          print("Fruta não encontrada")
+
+# 5) Altere o exercicio 4 para adicionar um preço aos itens comprados, mantendo uma conta com o valor gasto pelo usuário, e no fim, imprima o valor total e os itens na cesta de compras.
+# Utilize coleções para mantes o conteudo da cesta de compras.
+# Opcional: Mantenha o terminal limpo sempre que possivel.
+
+
 opcao = 0
-cesta = ""
-cesta_pos = 0
+cesta = []
+cesta_pre = 0
 fruta = 0
 
 while opcao < 3:
@@ -31,20 +63,24 @@ while opcao < 3:
 
     if opcao == 1:
         print(cesta)
+        print(f"Total: {cesta_pre}")
     
 
     if opcao == 2:
         fruta = int(input("Menu de frutas:\nDigite a opção que deseja adicionar:\n1 - Banana\n2 - Melancia\n3 - Morango\n"))
         
         if fruta == 1:
-           cesta = cesta + "Banana, " 
+           cesta.insert(len(cesta),"Banana") 
+           cesta_pre +=7
            print("Banana adicionada com sucesso")
+
         elif fruta == 2:
-           cesta += "Melancia, " 
+           cesta.insert(len(cesta),"Melancia") 
+           cesta_pre +=20
            print("Melancia adicionada com sucesso")
         elif fruta == 3:
-           cesta = "Morango, " 
+           cesta.insert(len(cesta),"Morango") 
+           cesta_pre +=5
            print("Morango adicionada com sucesso")
         else:
            print("Fruta não encontrada")
-    
