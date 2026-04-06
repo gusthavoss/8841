5fcdcac3261a8fc4284ef9bdb7cfca3c93623c1a
Gusthavo Silva <gusthavo.silva@nubank.com.br>
2026-04-06 15:23:18 -0300

    Aula 09 commit

---
+++ Aula09/exercicios.py
@@ -0,0 +1,19 @@
+# Ex 19 (Projeto 01):
+# Utilizando todo o conhecimento que você aprendeu no treinamento, crie uma aplicaçao capaz de simular todo funcionamento de um 
+# caixa eletronico. O caixa eletronico deverá ter as seguintes funçoes:
+# 1 - Novos clientes devem ser capazes de se registrar via caixa eletronico.
+# 2 - Usuários devem ser capazer de logar no caixa eletronico.
+# 3 - Usuários devem ser capazes de checar seu saldo no caixa eletrônico.
+# 4 - Usuários devem ser capazes de realizar depósitos no caixa eletronico. 
+# 5 - Usuários devem ser capazes de realizar saques no caixa eletronico.
+# 6 - O caixa eletrônico só deverá funcionar das 06:00 as 22:00, com um limite de saque de R$ 2.000,00 se usado antes das 08:00 ou 
+# depois das 20:00. (Simule o horário de atendimento aleatóriamente APENAS PARA TESTES)
+
+# Faça o uso de Classes, funç±oes, tratamento de erros e banco de dados no seu código.
+
+# -> Clientes podem ser criados a partir de uma classe.
+# -> Informações de clientes, como nome, usuario, senha e saldo podem ser armazenados em um banco de dados.
+# -> Podemos criar funções pra executar as ações do caixa eletrônico.
+# -> Podemos fazer o uso de multiplos arquivos pra armazenar trechos de código diferente, como um arquivo pra funções, um pra Classe, um pra banco de dados, um pra autenticação...
+# -> A aplicação será executada em um ambiente virtual próprio.
+
